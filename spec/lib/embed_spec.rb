3152ad7f9df8abdd205f77b1d7f4b05cdd19a251
hannesg <hag@informatik.uni-kiel.de>
2010-10-06 02:05:58 +0200

    added embed spec

---
+++ spec/lib/embed_spec.rb
@@ -0,0 +1,41 @@
+# -*- encoding : utf-8 -*-
+require File.expand_path(File.join(File.dirname(__FILE__),"../helper"))
+
+describe Splash::Embed do
+  
+  it "should be saveable as document attribute" do
+    
+    class ICQ
+      
+      include Splash::Embed
+      
+      attribute "uin"
+      
+    end
+    
+    
+    class Customer
+      
+      include Splash::Document
+      
+      attribute 'icq', ICQ
+      
+      attribute 'friends', Splash::Collection.of(ICQ) do
+        
+        default &:new
+        
+      end
+      
+    end
+    
+    c = Customer.new( 'icq' => ICQ.new('uin'=>1337) )
+    
+    c.friends << ICQ.new('uin' => 666)
+    c.friends << ICQ.new('uin' => 888)
+    
+    c.store!
+    
+    
+  end
+  
+end
